98a2b3bcba99ae2353560e600c961385f71526ba
Elton Z <poptako9@gmail.com>
2022-05-19 15:35:05 -0700

    Fixed file issue
    Needs to be a local file and when we implement it into a server, we need
    to account for that.

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const DiscordJS = require("discord.js")
 const WOKCommands = require("wokcommands")
+const path = require('path')
+const schedule = require("./commands/schedule")
+const { default: CommandHandler } = require("wokcommands/dist/CommandHandler")
 require("dotenv").config()
 
 const client = new DiscordJS.Client({intents: ["GUILDS","GUILD_MESSAGES"]})
 
 client.on("ready", () =>{
     console.log('The bot is ready')
+    const dbOptions = {
+        keepAlive: true 
+    }
     new WOKCommands(client, {
-        commandsDir: "commands",
-        showWarns: false
-    }).setMongoPath(process.env.MONGO_URI)
+        commandsDir: path.join("C:","Users","popta","DiscordReminderBot","node_modules","wokcommands","dist","commands"),
+        /*commandsDir: "C:\Users\popta\DiscordReminderBot\node_modules\wokcommands\dist\commands",*/
+        dbOptions,
+        showWarns: false,
+        mongoUri: ""
+    })
 })
 
 client.login(process.env.TOKEN) 
